95b54e84fc4b0d4b5e5aa3cab4a148a859f1dd86
Taewan Kim <taekim.dev@gmail.com>
2024-01-07 15:53:29 +0800

    add sample api and middleware log

--- app.vue
+++ app.vue
@@ -1,7 +1,27 @@
 <template>
   <div>
     <p>Greeting</p>
-    <p>{{ $t('greeting') }}</p>
+    <p>{{ $t("greeting") }}</p>
+    <p>{{ greeting }}</p>
     <NuxtWelcome />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      greeting: 'Loading...'
+    }
+  },
+  async mounted(){
+    try {
+      const response = await fetch('api/hello');
+      const data = await response.json();
+      this.greeting = data.message;
+    } catch (error) {
+      console.error('Error fetching greeting: ', error);
+      this.greeting = 'Error fetching greeting';
+    }
+  }
+}
+</script>
